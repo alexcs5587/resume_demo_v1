d2dc473f92064fa7af11bb739fe92231b3d708b5
alexcs5587 <alexcs5587@gmail.com>
2020-10-18 23:54:38 +0800

    Updates

---
+++ precache-manifest.d002c5ba829480e4f78ca0a2a1a8eee0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd48210d32d9d07fc9ad8b7c212242aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8006bdd87ad5681609fd",
+    "url": "/static/css/main.caf7e56b.chunk.css"
+  },
+  {
+    "revision": "2c99f04162010549facb",
+    "url": "/static/js/2.725c8ee1.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.725c8ee1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8006bdd87ad5681609fd",
+    "url": "/static/js/main.485fd8ec.chunk.js"
+  },
+  {
+    "revision": "9d56b0ea2705241215be",
+    "url": "/static/js/runtime-main.cf8c75f0.js"
+  }
+]);
